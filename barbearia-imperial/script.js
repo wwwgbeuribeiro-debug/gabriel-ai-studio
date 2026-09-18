00f347eec0cc55af0c43cbc618ce4540f39264c8
Gabriel Ribeiro <wwwgbeuribeiro-debuggit>
2026-09-18 19:19:51 -0300

    chore: remove projetos e arquivos de teste antigos

--- barbearia-imperial/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.getElementById('nav');
-
-    menuToggle.addEventListener('click', () => {
-        const isOpen = nav.style.display === 'flex';
-        nav.style.display = isOpen ? 'none' : 'flex';
-    });
-});
